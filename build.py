188a37a68b7f19376d5dc92d63082b80bb2f1d85
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-01-21 22:07:57 +0900

    docs: fix comments

--- build.py
+++ build.py
@@ -12,16 +12,16 @@ __all__ = ("build",)
 
 
 def build(setup_kwargs: Dict[str, Any]) -> None:
-    """Build the package."""
+    """Build C-extensions."""
     skbuild.setup(**setup_kwargs, script_args=["build_ext"])
 
     src_dir = Path(skbuild.constants.CMAKE_INSTALL_DIR()) / "hello"
     dest_dir = Path("hello")
 
-    # Delete C-extentions copied in previous runs, just in case.
+    # Delete C-extensions copied in previous runs, just in case.
     remove_files(dest_dir, "**/*.so")
 
-    # Copy built C-extentions back to the project.
+    # Copy built C-extensions back to the project.
     copy_files(src_dir, dest_dir, "**/*.so")
 
 
@@ -33,6 +33,7 @@ def remove_files(target_dir: Path, pattern: str) -> None:
         else:
             path.unlink()
         distutils_log.info(f"removed {path}")  # type: ignore[call-arg]
+        # will be fixed in mypy 0.800, https://github.com/python/typeshed/pull/4573
 
 
 def copy_files(src_dir: Path, dest_dir: Path, pattern: str) -> None:
@@ -40,11 +41,13 @@ def copy_files(src_dir: Path, dest_dir: Path, pattern: str) -> None:
     for src in src_dir.glob(pattern):
         dest = dest_dir / src.relative_to(src_dir)
         if src.is_dir():
+            # NOTE: inefficient if subdirectories also match to the pattern.
             copy_files(src, dest, "*")
         else:
             dest.parent.mkdir(parents=True, exist_ok=True)
             shutil.copy2(src, dest)
             distutils_log.info(f"copied {src} to {dest}")  # type: ignore[call-arg]
+            # will be fixed in mypy 0.800, https://github.com/python/typeshed/pull/4573
 
 
 if __name__ == "__main__":
